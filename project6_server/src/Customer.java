cfd171ccf80d0f4bd4cced3c98f4ebe17da88688
pln324 <47726343+pln324@users.noreply.github.com>
2020-05-06 23:35:07 -0500

    added customer class to server

---
+++ project6_server/src/Customer.java
@@ -0,0 +1,16 @@
+
+
+public class Customer {
+	public String username;
+	public String password;
+	
+	public Customer() {
+		username = "";
+		password = "";
+	}
+	
+	public Customer(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+}
